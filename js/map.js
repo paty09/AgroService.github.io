eb41148be9a8901078de3dc50ea37e8d25b46953
RigoCab-hack <61894331+RigoCab-hack@users.noreply.github.com>
2020-03-07 21:02:39 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -13,16 +13,6 @@ var map = new H.Map(document.getElementById('map'),
 
 var svgMarker = new  H.map.Icon('./images/tractor.png');
 
-<<<<<<< HEAD
 var marker = new H.map.Marker({lat:21.09413, lng:100.56038}, {icon: svgMarker});
-=======
-
-var icon = new H.map.Icon(svgMarkup),
-    coords = {lat: 52.53075, lng: 13.3851},
-    marker = new H.map.Marker(coords, {icon: icon});
-
-var marker = new H.map.Marker({lat:21.15824, lng:-100.93362}, {icon: svgMarker});
-
->>>>>>> fcd6ed0c74f4fae93f6d10cdc253bfa3c3462fca
 
 map.addObject(marker);
